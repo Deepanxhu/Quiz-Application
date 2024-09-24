7017638d11f3997fcb7ec341b0fd2541e700c61f
Deepanxhu <deepanshu1.yadav@gmail.com>
2024-09-24 12:24:45 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require("dotenv").config();
+const dbConfig = require('./config/dbConfig');
+const cors = require('cors');
+const app = express();
+const usersRoute = require('./routes/usersRoute');
+const examsRoute = require('./routes/examsRoute');
+const reportsRoute = require("./routes/reportsRoute");
+
+dbConfig();
+
+app.use(cors());
+
+app.use(express.json());
+app.use("/api/users", usersRoute);
+app.use("/api/exams", examsRoute);
+app.use("/api/reports", reportsRoute);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
